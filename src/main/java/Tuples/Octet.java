95ddd65816632a6372b13111ae6d588cf39c5b5d
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-04-22 23:23:39 +0300

    Initial Commit

---
+++ src/main/java/Tuples/Octet.java
@@ -0,0 +1,63 @@
+package Tuples;/* Created by oguzkeremyildiz on 22.04.2020 */
+
+/**
+ * @author oguzkeremyildiz
+ *
+ */
+
+public class Octet<A, B, C, D, E, F, G, H> {
+    A a;
+    B b;
+    C c;
+    D d;
+    E e;
+    F f;
+    G g;
+    H h;
+    public Octet(A a, B b, C c, D d, E e, F f, G g, H h) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+        this.g = g;
+        this.h = h;
+    }
+    public A getA() {
+        return a;
+    }
+    public B getB() {
+        return b;
+    }
+    public C getC() {
+        return c;
+    }
+    public D getD() {
+        return d;
+    }
+    public E getE() {
+        return e;
+    }
+    public F getF() {
+        return f;
+    }
+    public G getG() {
+        return g;
+    }
+    public H getH() {
+        return h;
+    }
+    @Override
+    public int hashCode() {
+        return a.hashCode() ^ b.hashCode() ^ c.hashCode() ^ d.hashCode() ^ e.hashCode() ^ f.hashCode() ^ g.hashCode() ^ h.hashCode();
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Octet)) {
+            return false;
+        }
+        Octet<A, B, C, D, E, F, G, H> octet = (Octet<A, B, C, D, E, F, G, H>) o;
+        return this.a.equals(octet.getA()) && this.b.equals(octet.getB()) && this.c.equals(octet.getC()) && this.d.equals(octet.getD()) && this.e.equals(octet.getE()) && this.f.equals(octet.getF()) && this.g.equals(octet.getG()) && this.h.equals(octet.getH());
+    }
+}
---
+++ src/main/java/Tuples/Septet.java
@@ -0,0 +1,58 @@
+package Tuples;/* Created by oguzkeremyildiz on 22.04.2020 */
+
+/**
+ * @author oguzkeremyildiz
+ *
+ */
+
+public class Septet<A, B, C, D, E, F, G> {
+    A a;
+    B b;
+    C c;
+    D d;
+    E e;
+    F f;
+    G g;
+    public Septet(A a, B b, C c, D d, E e, F f, G g) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+        this.g = g;
+    }
+    public A getA() {
+        return a;
+    }
+    public B getB() {
+        return b;
+    }
+    public C getC() {
+        return c;
+    }
+    public D getD() {
+        return d;
+    }
+    public E getE() {
+        return e;
+    }
+    public F getF() {
+        return f;
+    }
+    public G getG() {
+        return g;
+    }
+    @Override
+    public int hashCode() {
+        return a.hashCode() ^ b.hashCode() ^ c.hashCode() ^ d.hashCode() ^ e.hashCode() ^ f.hashCode() ^ g.hashCode();
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Septet)) {
+            return false;
+        }
+        Septet<A, B, C, D, E, F, G> septet = (Septet<A, B, C, D, E, F, G>) o;
+        return this.a.equals(septet.getA()) && this.b.equals(septet.getB()) && this.c.equals(septet.getC()) && this.d.equals(septet.getD()) && this.e.equals(septet.getE()) && this.f.equals(septet.getF()) && this.g.equals(septet.getG());
+    }
+}
