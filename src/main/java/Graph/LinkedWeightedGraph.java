f5d017e45d1ec0cbf4e1ba9af3326cdc3650c1c1
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-04-27 18:25:58 +0300

    Initial Commit

---
+++ src/main/java/Graph/LinkedWeightedGraph.java
@@ -0,0 +1,90 @@
+package Graph;/* Created by oguzkeremyildiz on 27.04.2020 */
+
+import Tuples.Pair;
+
+import java.util.*;
+
+/**
+ * @author oguzkeremyildiz
+ * @version 1.0.0
+ */
+
+public class LinkedWeightedGraph<Symbol> {
+    private LinkedHashMap<Symbol, LinkedList<Pair<Symbol, Integer>>> edgeList;
+    private LinkedHashSet<Symbol> vertexList;
+
+    public LinkedWeightedGraph() {
+        edgeList = new LinkedHashMap<>();
+        vertexList = new LinkedHashSet<>();
+    }
+
+    public LinkedWeightedGraph(LinkedHashSet<Symbol> vertexList) {
+        edgeList = new LinkedHashMap<>();
+        this.vertexList = vertexList;
+    }
+
+    public LinkedList<Pair<Symbol, Integer>> getFirst() {
+        Map.Entry<Symbol, LinkedList<Pair<Symbol, Integer>>> entry = edgeList.entrySet().iterator().next();
+        Symbol key = entry.getKey();
+        return edgeList.get(key);
+    }
+
+    public boolean isEmpty() {
+        return edgeList.isEmpty();
+    }
+
+    public boolean containsKey(Symbol element) {
+        return edgeList.containsKey(element);
+    }
+
+    public boolean containsValue(LinkedList<Pair<Symbol, Integer>> list) {
+        return edgeList.containsValue(list);
+    }
+
+    public void addDirectedEdge(Symbol from, Symbol to, int length) {
+        vertexList.add(from);
+        vertexList.add(to);
+        if (!edgeList.containsKey(from)){
+            edgeList.put(from, new LinkedList<>());
+            edgeList.get(from).addFirst(new Pair<>(to, length));
+        } else {
+            edgeList.get(from).add(new Pair<>(to, length));
+        }
+    }
+
+    public void addUndirectedEdge(Symbol from, Symbol to, int lengthFrom, int lengthTo) {
+        addDirectedEdge(from, to, lengthTo);
+        addDirectedEdge(to, from, lengthFrom);
+    }
+
+    public Set<Symbol> getKeySet() {
+        return edgeList.keySet();
+    }
+
+    public int size() {
+        return edgeList.size();
+    }
+
+    public void put(Symbol index, LinkedList<Pair<Symbol, Integer>> list) {
+        vertexList.add(index);
+        edgeList.put(index, list);
+        if (list.size() > 0) {
+            for (Pair<Symbol, Integer> element : list) {
+                vertexList.add(element.getKey());
+            }
+        }
+    }
+
+    public void replace(Symbol key, LinkedList<Pair<Symbol, Integer>> value) {
+        edgeList.replace(key, value);
+        if (value.size() > 0) {
+            for (Pair<Symbol, Integer> element : value) {
+                vertexList.add(element.getKey());
+            }
+        }
+    }
+
+    public LinkedList<Pair<Symbol, Integer>> get(Symbol index) {
+        return edgeList.get(index);
+    }
+}
